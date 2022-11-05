486b6543be519007b7b68db105f4abf936624de3
johnthinguyen <ngocthiazaaaa12@gmail.com>
2022-11-05 23:49:45 +0700

    state_react

---
+++ src/Glasses/Glasses.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Header from './Components/Header'
+
+export default class Glasses extends Component {
+    render() {
+        return (
+            <div className='home'>
+                <Header />
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Glasses from './Glasses/Glasses';
+import css from "./css/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Glasses />
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
